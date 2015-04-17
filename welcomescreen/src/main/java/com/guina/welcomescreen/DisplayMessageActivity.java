be3f9d59f05f86790efe3fdaf027d763535ccaf1
mguina <mike.guina@gmail.com>
2015-04-17 18:26:14 +0100

    Following this guide, having success
    https://developer.android.com/training/basics/firstapp/building-ui.html

--- welcomescreen/src/main/java/com/guina/welcomescreen/DisplayMessageActivity.java
+++ welcomescreen/src/main/java/com/guina/welcomescreen/DisplayMessageActivity.java
@@ -1,9 +1,11 @@
 package com.guina.welcomescreen;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -11,7 +13,12 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Welcome.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
     }
 
 
--- welcomescreen/src/main/java/com/guina/welcomescreen/Welcome.java
+++ welcomescreen/src/main/java/com/guina/welcomescreen/Welcome.java
@@ -19,7 +19,7 @@ import android.widget.EditText;
  * @see SystemUiHider
  */
 public class Welcome extends Activity {
-    public final static String EXTRA_MESSAGE = "com.guina.GitTest2.MESSAGE";
+    public final static String EXTRA_MESSAGE = "com.guina.welcome.MESSAGE";
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -55,49 +55,49 @@ public class Welcome extends Activity {
         setContentView(R.layout.activity_welcome);
 
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
+        // final View contentView = findViewById(R.id.fullscreen_content);
 
         // Set up an instance of SystemUiHider to control the system UI for
         // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+       // mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
+       // mSystemUiHider.setup();
+       // mSystemUiHider
+       //         .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
                     // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
+       //             int mControlsHeight;
+       //             int mShortAnimTime;
 
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+       //             @Override
+       //             @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+       //             public void onVisibilityChange(boolean visible) {
+       //                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
                             // If the ViewPropertyAnimator API is available
                             // (Honeycomb MR2 and later), use it to animate the
                             // in-layout UI controls at the bottom of the
                             // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
+       //                     if (mControlsHeight == 0) {
+       //                         mControlsHeight = controlsView.getHeight();
+       //                     }
+       //                     if (mShortAnimTime == 0) {
+       //                         mShortAnimTime = getResources().getInteger(
+       //                                 android.R.integer.config_shortAnimTime);
+       //                     }
+       //                     controlsView.animate()
+       //                            .translationY(visible ? 0 : mControlsHeight)
+       //                            .setDuration(mShortAnimTime);
+       //                 } else {
                             // If the ViewPropertyAnimator APIs aren't
                             // available, simply show or hide the in-layout UI
                             // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
+       //                     controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        //                }
 
-                        if (visible && AUTO_HIDE) {
+       //                 if (visible && AUTO_HIDE) {
                             // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
+       //                     delayedHide(AUTO_HIDE_DELAY_MILLIS);
+       //                 }
+       //             }
+       //         });
 
         // Set up the user interaction to manually show or hide the system UI.
         contentView.setOnClickListener(new View.OnClickListener() {
